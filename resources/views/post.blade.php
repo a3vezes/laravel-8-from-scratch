3a9d381735a6596d9e1635c1e6cfe114789ef3fb
Arthur Ariza <tucaariza@gmail.com>
2021-08-03 21:11:07 -0300

    Section 1 - Section 3

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('content')
+  <article>
+      <h1>
+          {{ $post->title }}
+      </h1>
+
+      <div>
+          {!! $post->body !!}
+      </div>
+
+  </article>
+  <a href="/posts">Go Back</a>
+@endsection
